7b43f2e42cf1d266b2b2df52d3eb23188bc445f1
theFrantic <whilchyjd@gmail.com>
2014-11-29 01:14:55 +0000

    Added a button and a simple action

--- src/FirstApp/FirstApp/app/src/main/java/firstapp/whilchy/com/firstapp/MainActivity.java
+++ src/FirstApp/FirstApp/app/src/main/java/firstapp/whilchy/com/firstapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+
+        Button firstButton = (Button) findViewById(R.id.buttonClickOnMe);
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstTextView.setText("Hell Yeahhh!!");
+            }
+        });
+
     }
 
 
