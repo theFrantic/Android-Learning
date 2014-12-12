f74574dbf80fbdb12c03345beffbb01f8cb44009
theFrantic <whilchyjd@gmail.com>
2014-12-12 22:43:58 +0000

    Fragmet Layouts: Tutorial finished

---
+++ src/FragmentLayouts/app/src/main/java/app/fragmentlayouts/whilchy/com/fragmentlayouts/FragmentLayout.java
@@ -0,0 +1,16 @@
+package app.fragmentlayouts.whilchy.com.fragmentlayouts;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class FragmentLayout extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_layout);
+    }
+}
